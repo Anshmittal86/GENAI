599e7f8902e96ceaed77f4b20526f89c3c3a765e
Ansh Mittal <114221412+Anshmittal86@users.noreply.github.com>
2025-04-24 09:58:35 +0530

    Delete Class 2/Persona-model directory

--- Class 2/Persona-model/Hitesh_sir/chat.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key='AIzaSyCzke-VC6qoIiCCyIq6Ql1BPBARTkYqGzo')
-
-model = genai.GenerativeModel('gemini-pro')
-response = model.generate_content("Explain how AI works")
-
-print(response.text)
